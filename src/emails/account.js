e4bff3b0b1ffdba8343cf60cf5652715810dfb7c
Sonal Gupta <sonalgupta276@gmail.com>
2020-07-19 19:29:04 +0530

    Initial commit

---
+++ src/emails/account.js
@@ -0,0 +1,25 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail =(email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'sonalgupta276@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welome to the app, ${name}. Let me know how you get along with the app`
+    })
+}
+
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'sonalgupta276@gmail.com',
+        subject: 'Sorry to see you go',
+        text: `Goodbye, ${name}. I hope to see you back soon.`
+    })
+}
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail
+}
---
+++ src/middleware/auth.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token' : token })
+        if(!user) {
+            throw new Error()
+        }
+        req.token = token
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate'})
+    }
+}
+
+module.exports = auth
+
+
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
+// })
+// without middleware : new request -> run route handler
+// with middleware : new request -> do something -> run route handler
